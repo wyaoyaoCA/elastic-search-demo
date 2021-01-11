486530e5c7b596271d13811c76fd4d5c103f7a59
wyaoyao <wangyaoyao.cosmo@haier.com>
2021-01-11 13:40:56 +0800

    索引文档

--- es-rest-high-level-client/src/main/java/study/wyy/esclient/high/document/BuildJsonDocumentTest.java
+++ es-rest-high-level-client/src/main/java/study/wyy/esclient/high/document/BuildJsonDocumentTest.java
@@ -1,12 +1,22 @@
 package study.wyy.esclient.high.document;
 
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.DocWriteRequest;
+import org.elasticsearch.action.DocWriteResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.support.WriteRequest;
+import org.elasticsearch.action.support.replication.ReplicationResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.VersionType;
+import org.elasticsearch.rest.RestStatus;
 import org.junit.Test;
+import study.wyy.esclient.high.BaseTest;
 
 import java.io.IOException;
 import java.util.Date;
@@ -18,13 +28,14 @@ import java.util.Map;
  * @description
  * @date 2021/1/8 16:32
  */
-public class BuildJsonDocumentTest {
+@Slf4j
+public class BuildJsonDocumentTest extends BaseTest {
 
     /****
-     * 直接使用字符串形式
+     * 直接使用字符串形式，测试同步执行，和响应结果的解析
      */
     @Test
-    public void testString(){
+    public void testString() throws IOException {
         // 索引的名字
         IndexRequest request = new IndexRequest("books");
         // 设置文档id, 也可以不设置es会自动生成一个
@@ -45,16 +56,49 @@ public class BuildJsonDocumentTest {
         // request.timeout("1s");
         // 设置超时策略
         request.setRefreshPolicy(WriteRequest.RefreshPolicy.WAIT_UNTIL);
+        // 设置版本
+        //request.version(2);
+        // 设置版本类型
+        //request.versionType(VersionType.EXTERNAL);
+        // 设置操作类型
+        //request.opType("create");
+        request.opType(DocWriteRequest.OpType.CREATE);
 
+        // 同步执行执行请求
+        try {
+            IndexResponse response = client.index(request, RequestOptions.DEFAULT);
+            // 响应状态
+            RestStatus status = response.status();
+            log.info("响应状态: {}",status);
+            // 获取文档id
+            String id = response.getId();
+            // 获取文档索引
+            String index = response.getIndex();
+            log.info("文档索引库: {}; 文档id: {}",index,id);
+            // 获取结果: 枚举值：创建，删除，更新，not find
+            // 对于此处来说，只能是创建或者是更新，es当id存在的时候就是执行更新，id不存在就是创建，和restAPI是一样的
+            DocWriteResponse.Result result = response.getResult();
+            log.info("响应结果: {}",result);
+            // 获取分片信息
+            ReplicationResponse.ShardInfo shardInfo = response.getShardInfo();
+            log.info("分片信息: {}",shardInfo);
+            // 获取文档版本
+            long version = response.getVersion();
+            log.info("文档版本号: {}",version);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            client.close();
+        }
 
     }
 
     /****
-     * 使用Map构建
+     * 使用Map构建，并测试异步执行
      *
      */
-    @Test
-    public void testMap(){
+    public static void testMap(){
         // 索引的名字
         IndexRequest request = new IndexRequest("books");
         // 设置文档id, 也可以不设置es会自动生成一个
@@ -68,14 +112,30 @@ public class BuildJsonDocumentTest {
         mappings.put("publish_time",new Date());
         mappings.put("description","让你的Java程序更快、更稳定。深入剖析软件设计层面、代码层面、JVM虚拟机层面的优化方法");
         request.source(mappings);
+
+        // 异步执行
+        client.indexAsync(request, RequestOptions.DEFAULT, new ActionListener<IndexResponse>() {
+            // 执行成功的回调
+            @Override
+            public void onResponse(IndexResponse indexResponse) {
+                log.info("成功");
+
+            }
+            // 执行失败的回调
+            @Override
+            public void onFailure(Exception e) {
+                log.info("失败");
+            }
+        });
+
     }
 
     /****
-     * 使用Map构建
+     * 使用XContentBuilder构建
      *
      */
     @Test
-    public void testMap2() throws IOException {
+    public void testXContentBuilder() throws IOException {
         // 索引的名字
         IndexRequest request = new IndexRequest("books");
         // 设置文档id, 也可以不设置es会自动生成一个
@@ -94,7 +154,12 @@ public class BuildJsonDocumentTest {
         // endObject 可以理解为json中的}
         xContentBuilder.endObject();
         request.source(xContentBuilder);
+        IndexResponse index = client.index(request, RequestOptions.DEFAULT);
+        client.close();
+    }
 
+    public static void main(String[] args) {
+        testMap();
     }
 
 }
